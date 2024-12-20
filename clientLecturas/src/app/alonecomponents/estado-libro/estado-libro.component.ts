dc5bccde0607589e2cfc0f2e18a44377afef46f7
Ignacio (jit87) <ignaciotejada87@gmail.com>
2024-12-20 20:50:54 +0100

    upload 3

--- clientLecturas/src/app/alonecomponents/estado-libro/estado-libro.component.ts
+++ clientLecturas/src/app/alonecomponents/estado-libro/estado-libro.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-estado-libro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './estado-libro.component.html',
   styleUrl: './estado-libro.component.css'
 })
