8c9e836efaf0a5829f99596483b68ba93104a43f
Ignacio (jit87) <ignaciotejada87@gmail.com>
2025-11-26 22:25:12 +0100

    Fix rederización comentarios, add boton eliminar comentario

--- clientLecturas/src/app/pages/contentpages/social/social.component.ts
+++ clientLecturas/src/app/pages/contentpages/social/social.component.ts
@@ -6,6 +6,41 @@ import { AbstractLecturasBBDDService } from '../../../abstracts/AbstractLecturas
 import { AbstractAuthService } from '../../../abstracts/AbstractAuthService';
 
 
+interface ComentarioVista extends ComentarioModel {
+  nombreUsuario: string;
+  imagenUsuario: string;
+}
+
+interface PostLibro {
+  APIid: string;
+  _id: string;
+  _idUsuario: string;
+  nombreUsuario: string;
+  imagenUsuario: string;
+  titulo: string;
+  autores: string[];
+  editor: string;
+  descripcion: string;
+  imagen: string;
+  resena: string;
+  tipo: "libro";
+  comentarios: ComentarioVista[];
+}
+
+interface PostResena {
+  APIid: string;
+  _id: string;
+  _idUsuario: string;
+  nombreUsuario: string;
+  imagenUsuario: string;
+  resena: string;
+  titulo: string;
+  tipo: "resena";
+  comentarios: ComentarioVista[];
+}
+
+
+
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
@@ -33,6 +68,7 @@ export class SocialComponent {
     private _lecturasBBDDService: AbstractLecturasBBDDService,
     private _authService: AbstractAuthService
   ) {
+    this.getActividad();
   }
 
 
@@ -60,47 +96,39 @@ export class SocialComponent {
 
   //MURO
   getActividad() {
+    this.cargados = false;
     this.posts = [];
     this._lecturasBBDDService.getListLibrosUsuarios().subscribe(
       (resp) => {
+        //let total = resp.length;
+        // let procesados = 0;
         resp.forEach((libro: any) => {
-          let postLibros = {
+          const postLibros: PostLibro = {
             APIid: libro.APIid,
             _id: libro._id,
             _idUsuario: libro._idUsuario,
-            nombreUsuario: "",
-            imagenUsuario: "",
+            nombreUsuario: '',
+            imagenUsuario: '',
             titulo: libro.titulo,
             autores: libro.autores,
             editor: libro.editor,
-            descripcion: libro.descripcion || "",
-            imagen: libro.imagen || "default.png",
-            resena: libro.resena || "",
-            tipo: "libro",
-            comentarios: [] as {
-              _idUsuario: string;
-              nombreUsuario: string;
-              imagenUsuario: string;
-              texto: string;
-              fecha: Date;
-            }[]
+            descripcion: libro.descripcion || '',
+            imagen: libro.imagen || 'default.png',
+            resena: libro.resena || '',
+            tipo: 'libro',
+            comentarios: [] as ComentarioVista[]
           };
-          let postResena = {
+
+          const postResena: PostResena = {
             APIid: libro.APIid,
             _id: libro._id,
             _idUsuario: libro._idUsuario,
-            nombreUsuario: "",
-            imagenUsuario: "",
-            resena: libro.resena || "",
+            nombreUsuario: '',
+            imagenUsuario: '',
+            resena: libro.resena || '',
             titulo: libro.titulo,
-            tipo: "resena",
-            comentarios: [] as {
-              _idUsuario: string;
-              nombreUsuario: string;
-              imagenUsuario: string;
-              texto: string;
-              fecha: Date;
-            }[]
+            tipo: 'resena',
+            comentarios: [] as ComentarioVista[]
           };
           this._authService.getUserById(libro._idUsuario).subscribe(
             (usuario: any) => {
@@ -113,63 +141,21 @@ export class SocialComponent {
               console.log(err);
             }
           );
-          //Si el libro no tiene reseña, es que se ha añadido recientemente, luego se muestra la actualización del libro
-          //Además obtenemos los comentarios asociados al libro
-          if (postLibros.resena == "") {
-            this._lecturasBBDDService.getComentarios(postLibros._id, postLibros.tipo).subscribe(
-              (resp: any) => {
-                if (resp != "") {
-                  postLibros.comentarios = resp;
-                  postLibros.comentarios.forEach((comentario, index) => {
-                    this._authService.getUserById(comentario._idUsuario).subscribe({
-                      next: (usuario) => {
-                        postLibros.comentarios[index].nombreUsuario = usuario.nombre;
-                        postLibros.comentarios[index].imagenUsuario = usuario.imagen;
-                      },
-                      error: (err) => console.log(err)
-                    });
-                  });
-                }
-              },
-              (err) => {
-                console.log(err);
-              }
-            );
-            this.posts.push(postLibros);
-          }
-          //Si la propiedad resena no está vacía se pasarán los datos a la parte de reseña (en función del tipo)
-          //Además obtenemos los comentarios asociados a la reseña
-          if (postResena.resena != "") {
-            this._lecturasBBDDService.getComentarios(postResena._id, postResena.tipo).subscribe(
-              (resp: any) => {
-                if (resp != "") {
-                  postResena.comentarios = resp;
-                  postResena.comentarios.forEach((comentario, index) => {
-                    this._authService.getUserById(comentario._idUsuario).subscribe({
-                      next: (usuario) => {
-                        postResena.comentarios[index].nombreUsuario = usuario.nombre;
-                        postResena.comentarios[index].imagenUsuario = usuario.imagen;
-                      },
-                      error: (err) => console.log(err)
-                    });
-                  });
-                }
-              },
-              (err) => {
-                console.log(err);
-              }
-            );
-            this.posts.push(postResena);
-          }
+          this.getComentarios(postLibros, postResena);
+          this.cargados = true;
+          /* if (procesados == total) {
+             this.cargados = true;
+           }*/
         });
       },
       (err) => {
         console.log(err);
       }
     );
-    this.cargados = true;
     return this.posts;
   }
+
+
 
   //SEGUIDOS Y SEGUIDORES
   getSeguidos() {
@@ -216,24 +202,34 @@ export class SocialComponent {
   }
 
 
-  guardarComentario(formulario: NgForm, tipo: string, _id: string) {
-    var nuevoComentario = new ComentarioModel();
-    nuevoComentario = {
+  guardarComentario(formulario: NgForm, tipo: string, _id: string, post: any) {
+    //Datos de comentario que guardamos en la colección comentarios
+    var nuevoComentarioBase = new ComentarioModel();
+    nuevoComentarioBase = {
       _idUsuario: this.idLogueado,
       _idLibro: _id,
       texto: formulario.value.comentarioTexto,
       fecha: new Date(),
       tipo: tipo
     }
-    this._lecturasBBDDService.addComentario(nuevoComentario).subscribe(
+    this._lecturasBBDDService.addComentario(nuevoComentarioBase).subscribe(
       (resp) => {
         console.log("Comentario añadido", resp);
+        //Necesitamos añadir los datos de imagen y nombre aparte porque estos no se guardan en la colección comentarios
+        //Copiamos en comentarioAmpliado el nuevoComentarioBase y le pasamos los datos de nombre y usuario, 
+        //luego se mostrará de manera reactiva el comentario con la imagen y nombre correspondientes
+        const comentarioAmpliado: ComentarioVista = {
+          ...nuevoComentarioBase,
+          nombreUsuario: this.nombreUsuario,
+          imagenUsuario: this.imagenUsuario
+        };
+        post.comentarios.push(comentarioAmpliado);
+        post.comentarios = [...post.comentarios];
       },
       (err) => {
         console.log(err);
       }
     )
-    this.getActividad();
   }
 
 
@@ -248,6 +244,59 @@ export class SocialComponent {
         console.log(err);
       }
     )
+  }
+
+
+  //Obtiene los comentarios asociados a cada post
+  getComentarios(postLibros: PostLibro, postResena: PostResena) {
+    //Si el libro no tiene reseña, es que se ha añadido recientemente, luego se muestra la actualización del libro
+    //Además obtenemos los comentarios asociados al libro
+    if (postLibros.resena == "") {
+      this._lecturasBBDDService.getComentarios(postLibros._id, postLibros.tipo).subscribe(
+        (resp: any) => {
+          if (resp != "") {
+            postLibros.comentarios = resp;
+            postLibros.comentarios.forEach((comentario, index) => {
+              this._authService.getUserById(comentario._idUsuario).subscribe({
+                next: (usuario) => {
+                  postLibros.comentarios[index].nombreUsuario = usuario.nombre;
+                  postLibros.comentarios[index].imagenUsuario = usuario.imagen;
+                },
+                error: (err) => console.log(err)
+              });
+            });
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+      this.posts.push(postLibros);
+    }
+    //Si la propiedad resena no está vacía se pasarán los datos a la parte de reseña (en función del tipo)
+    //Además obtenemos los comentarios asociados a la reseña
+    if (postResena.resena != "") {
+      this._lecturasBBDDService.getComentarios(postResena._id, postResena.tipo).subscribe(
+        (resp: any) => {
+          if (resp != "") {
+            postResena.comentarios = resp;
+            postResena.comentarios.forEach((comentario, index) => {
+              this._authService.getUserById(comentario._idUsuario).subscribe({
+                next: (usuario) => {
+                  postResena.comentarios[index].nombreUsuario = usuario.nombre;
+                  postResena.comentarios[index].imagenUsuario = usuario.imagen;
+                },
+                error: (err) => console.log(err)
+              });
+            });
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+      this.posts.push(postResena);
+    }
   }
 
 
