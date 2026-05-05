1f8ea773e50d80ac67fc2babdc99d04b944b28f6
Ignacio (jit87) <ignaciotejada87@gmail.com>
2026-05-05 14:40:47 +0200

    Update - libros.service.ts

--- clientLecturas/src/app/services/libros.service.ts
+++ clientLecturas/src/app/services/libros.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, shareReplay } from 'rxjs';
 import { environment } from '../environments/environment'
 import { AbstractLibrosService } from '../abstracts/AbstractLibrosService';
 
@@ -19,6 +19,9 @@ export class LibrosService extends AbstractLibrosService {
   titulo: string = "";
   estado: string = "";
 
+  private librosNuevos$?: Observable<any>;
+
+
   constructor(private http: HttpClient) { super(); }
 
   //Datos de la API
@@ -29,14 +32,24 @@ export class LibrosService extends AbstractLibrosService {
 
   //Devuelve novedades de libros 
   getLibrosNuevos(): Observable<any> {
-    return this.http.get(`${this.url}subject:fiction&printType=books&orderBy=newest&maxResults=${this.max}&key=${this.Google_API_KEY}`).pipe(
-      map((res: any) => {
-        //Si hay libros devolvemos un vector de libros con el tamaño determinado por max o los que haya devuelto la API
-        const items = res.items?.slice(0, this.max) || [];
-        //Sustituimos items por el vector de libros limitado por max 
-        return { ...res, items };
-      })
-    );
+    if (!this.librosNuevos$) {
+      this.librosNuevos$ = this.http.get(`${this.url}subject:fiction&printType=books&orderBy=newest&maxResults=${this.max}&key=${this.Google_API_KEY}`).pipe(
+        map((res: any) => {
+          //Si hay libros devolvemos un vector de libros con el tamaño determinado por max o los que haya devuelto la API
+          const items = res.items?.slice(0, this.max) || [];
+          //Sustituimos items por el vector de libros limitado por max 
+          return { ...res, items };
+        }),
+        shareReplay(1),
+        catchError(error => {
+          console.error("Error en la API", error);
+          this.librosNuevos$ = undefined;
+          return of({ items: [] });
+        }
+        )
+      );
+    }
+    return this.librosNuevos$;
   }
 
   getInfoLibroById(id: string): Observable<any> {
