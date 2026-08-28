f589822918f1577ad69b009742103ff5eaad4f0d
Ignacio (jit87) <ignaciotejada87@gmail.com>
2026-08-28 21:04:13 +0200

    fix environments

--- clientLecturas/src/app/environments/environment.prod.ts
+++ clientLecturas/src/app/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: window.location.origin,
-    Google_API_KEY: 'AIzaSyDnR9eHS8r3F0WYk5GayS4thengEFshJjU',
-    openaiApiKey: ''
+    Google_API_KEY: 'AIzaSyDnR9eHS8r3F0WYk5GayS4thengEFshJjU'
 };
